78a1a2b0cb879e45bd2ce15c248da9b00a589855
Ameet <writetoameet@gmail.com>
2020-03-29 17:32:27 -0700

    adding US state chart

--- datasync.js
+++ datasync.js
@@ -103,3 +103,32 @@ function getCounties() {
 }
 
 getCounties();
+
+
+
+function getStates() {
+
+  fetch('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv')
+  .then(res => res.text())
+  .then(body => {
+
+//date,state,fips,cases,deaths
+      const [headers, ...rows] = parse(body);
+      const [date,state,fips,cases,deaths] = headers;
+      var data = {};
+      rows.forEach(([date,state,fips,cases,deaths]) => {
+        if(!data[state]) data[state] = [];
+        data[state].push({ date: date, fips: fips, cases: cases, state: state, deaths: deaths, total: cases+deaths });
+        //data[+fips] = +cases;
+      });
+      //console.log(data);
+
+  var outputPathStates = path.join(WORKSPACE, MAIN_REPO, "docs/data/", "states.json");
+  fs.writeFileSync(outputPathStates, JSON.stringify(data, null, 2));
+  //fs.writeFileSync('./app/assets/data/states.json', JSON.stringify(data, null, 2));
+
+  });
+
+}
+
+getStates();
